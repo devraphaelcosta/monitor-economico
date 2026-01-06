1b8eefa6cb77c6f2d1d0a1996a7df792050d75dd
Raphael Valente <raphael194889@gmail.com>
2026-01-06 12:57:35 -0300

    Adicionar teste E2E validando título do painel e indicadores

--- frontend/monitor-economico-frontend/cypress/e2e/dashboard.cy.ts
+++ frontend/monitor-economico-frontend/cypress/e2e/dashboard.cy.ts
@@ -1,7 +1,14 @@
 describe('Dashboard Econômico', () => {
-  it('deve exibir os principais indicadores', () => {
+  it('deve exibir o painel e os principais indicadores', () => {
     cy.visit('http://localhost:4200');
 
+    // Valida o título principal da página
+cy.get('h1')
+  .should('be.visible')
+  .and('contain.text', 'Painel de Monitoramento Econômico');
+
+
+    // Valida os indicadores principais
     cy.contains('SELIC').should('be.visible');
     cy.contains('IPCA').should('be.visible');
     cy.contains('CÂMBIO').should('be.visible');
