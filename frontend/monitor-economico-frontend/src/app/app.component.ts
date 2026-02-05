910db00c15f5ba150b2a40b80545d6cdecd77a2e
Raphael Valente <raphael194889@gmail.com>
2026-02-05 00:11:03 -0300

    Inserindo Seta para voltar para home na pagina do painel e simulador

--- frontend/monitor-economico-frontend/src/app/app.component.ts
+++ frontend/monitor-economico-frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
