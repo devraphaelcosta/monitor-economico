ea30d21e544b09ac8e49879c75b4d2482cd79ed9
Raphael Valente <raphael194889@gmail.com>
2026-02-05 05:05:16 -0300

    Removendo Simulador

--- frontend/monitor-economico-frontend/src/app/app.routes.ts
+++ frontend/monitor-economico-frontend/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { SimuladorComponent } from './pages/simulador/simulador.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'painel', component: DashboardComponent },
-  { path: 'simulador', component: SimuladorComponent },
 ];
